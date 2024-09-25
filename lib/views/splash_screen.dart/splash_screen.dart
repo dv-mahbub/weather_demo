7c68ef1b2802079c1ec6bb8cdcb4f64cebc6ae16
dv-mahbub <dv.mahbub@gmail.com>
2024-09-25 23:33:22 +0600

    splash screen ui

--- lib/views/splash_screen.dart/splash_screen.dart
+++ lib/views/splash_screen.dart/splash_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:weather_demo/components/constants/colors.dart';
 import 'package:weather_demo/views/splash_screen.dart/animated_logo.dart';
+import 'package:stylish_text/stylish_text.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -13,7 +16,26 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: AnimatedLogo(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 250, child: AnimatedLogo()),
+            Text(
+              'Weather App',
+              style: TextStyle(
+                color: AppColors.primary,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Gap(65),
+            const TypewriterText(
+              'Loading...',
+              style: TextStyle(color: Colors.blue),
+            ),
+            const Gap(15),
+          ],
+        ),
       ),
     );
   }
