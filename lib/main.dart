6b09157a5de1f98e15094fe94f6c569041b6b7a5
dv-mahbub <dv.mahbub@gmail.com>
2024-09-26 23:25:51 +0600

    until time scified part

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_demo/views/homepage.dart';
 import 'package:weather_demo/views/splash_screen.dart/splash_screen.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      home: Homepage(),
     );
   }
 }
--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gap/gap.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:weather_demo/components/constants/images.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -10,6 +12,7 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  bool isTodaySelected = true;
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -18,9 +21,14 @@ class _HomepageState extends State<Homepage> {
       width: size.width,
       decoration: const BoxDecoration(
         gradient: LinearGradient(
-          colors: [Color(0xff97ABFF), Color(0xff123597)],
+          colors: [
+            Color(0xff97ABFF),
+            Color.fromARGB(255, 118, 139, 225),
+            Color(0xff123597)
+          ],
           begin: Alignment.centerLeft,
           end: Alignment.centerRight,
+          // stops: [0.40, 1],
         ),
       ),
       child: Scaffold(
@@ -36,6 +44,45 @@ class _HomepageState extends State<Homepage> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     ...locationPart(),
+                    ...headerPart(),
+                    ...daySelector(),
+                    Container(
+                      height: 130,
+                      width: 60,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.white.withOpacity(.4),
+                          width: 2,
+                        ),
+                        borderRadius: BorderRadius.circular(35),
+                        gradient: LinearGradient(
+                          colors: [
+                            Color(0xff97ABFF),
+                            Color.fromARGB(255, 109, 138, 253),
+                          ],
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          // stops: [0.40, 1],
+                        ),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(
+                            'Now',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          Image.asset(
+                            AppImages.slightTouchHappyDay,
+                            width: 55,
+                          ),
+                          Text(
+                            '13°',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
@@ -62,6 +109,80 @@ class _HomepageState extends State<Homepage> {
             const Text(
               'Current Location',
               style: TextStyle(color: Colors.white, fontSize: 15),
+            ),
+          ],
+        ),
+      ),
+    ];
+  }
+
+  List<Widget> headerPart() {
+    return [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            AppImages.partlyCloudy,
+            width: 135,
+            fit: BoxFit.fitWidth,
+          ),
+          const Gap(10),
+          Text(
+            '13°',
+            style: GoogleFonts.quicksand(
+              textStyle: const TextStyle(fontSize: 102, color: Colors.white),
+            ),
+          )
+        ],
+      ),
+      Text(
+        'Partly Cloud  -  H:17o  L:4o',
+        style: TextStyle(fontSize: 18, color: Colors.white),
+      ),
+    ];
+  }
+
+  List<Widget> daySelector() {
+    return [
+      Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                if (mounted) {
+                  setState(() {
+                    isTodaySelected = true;
+                  });
+                }
+              },
+              child: Text('Today'),
+              style: ButtonStyle(
+                padding: WidgetStatePropertyAll(
+                    EdgeInsets.symmetric(horizontal: 35)),
+                backgroundColor: isTodaySelected
+                    ? WidgetStatePropertyAll(Color(0xff7087D8))
+                    : WidgetStatePropertyAll(Color(0xff4D66B8)),
+                foregroundColor: WidgetStatePropertyAll(Colors.white),
+              ),
+            ),
+            Gap(5),
+            ElevatedButton(
+              onPressed: () {
+                if (mounted) {
+                  setState(() {
+                    isTodaySelected = false;
+                  });
+                }
+              },
+              child: Text('Next Day'),
+              style: ButtonStyle(
+                backgroundColor: !isTodaySelected
+                    ? WidgetStatePropertyAll(Color(0xff7087D8))
+                    : WidgetStatePropertyAll(Color(0xff4D66B8)),
+                foregroundColor: WidgetStatePropertyAll(Colors.white),
+              ),
             ),
           ],
         ),
