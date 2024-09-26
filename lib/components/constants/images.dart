6efbb5acd5fd712c62b91f7d7a5e051748cb0daf
dv-mahbub <dv.mahbub@gmail.com>
2024-09-26 21:06:38 +0600

    location part, weather images

---
+++ lib/components/constants/images.dart
@@ -0,0 +1,9 @@
+class AppImages {
+  static String imageFolder = 'assets/images';
+  static String logo = '$imageFolder/logo.png';
+  static String nightStrom = '$imageFolder/night_storm.png';
+  static String partlyCloudy = '$imageFolder/partly_cloudy.png';
+  static String partlyDayStrom = '$imageFolder/partly_day_storm.png';
+  static String rainy = '$imageFolder/rainy.png';
+  static String slightTouchHappyDay = '$imageFolder/slight_touch_happy_day.png';
+}
--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gap/gap.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -10,22 +12,60 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: RefreshIndicator(
-        onRefresh: () async {},
-        child: const CustomScrollView(
-          physics: AlwaysScrollableScrollPhysics(),
-          slivers: [
-            SliverFillRemaining(
-              hasScrollBody: false,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [],
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      height: size.height,
+      width: size.width,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Color(0xff97ABFF), Color(0xff123597)],
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: RefreshIndicator(
+          onRefresh: () async {},
+          child: CustomScrollView(
+            physics: const AlwaysScrollableScrollPhysics(),
+            slivers: [
+              SliverFillRemaining(
+                hasScrollBody: false,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ...locationPart(),
+                  ],
+                ),
               ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> locationPart() {
+    return [
+      const Text(
+        'Dhaka',
+        style: TextStyle(color: Colors.white, fontSize: 32),
+      ),
+      const Gap(12),
+      InkWell(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset('assets/svg/location.svg'),
+            const Gap(7),
+            const Text(
+              'Current Location',
+              style: TextStyle(color: Colors.white, fontSize: 15),
             ),
           ],
         ),
       ),
-    );
+    ];
   }
 }
